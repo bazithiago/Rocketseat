3fca069e7b2632293929f706859353127591f4e3
Thiago <thiagobazinga@gmail.com>
2020-06-02 17:56:49 -0300

    Módulo 1, exercício 1

--- introducaoJS.js
+++ introducaoJS.js
@@ -1,5 +1,16 @@
-let sexo = "M"
+let cadastro = {
+    nome: "Thiago",
+    rua: "Brigadeiro Luis Antonio",
+    numero: 1186,
+    bairro: "Bela Vista",
+    cidade: "São Paulo",
+    uf: "SP"
+}
 
-let resultado = (sexo === "M") ? "Masculino" : "Feminino"
+function enderecoUsuario(usuario) {
+    let resultado = (cadastro.nome == usuario) ? "O usuário mora em " + cadastro.cidade + " / " + cadastro.uf + ", no bairro " + cadastro.bairro + ", no endereço: " + cadastro.rua + ", nº " + cadastro.numero + "." : "Usuário não cadastrado";
 
-console.log(resultado)
+    return resultado
+}
+
+console.log(enderecoUsuario("Thiago"))
