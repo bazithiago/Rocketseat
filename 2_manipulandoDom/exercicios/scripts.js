fd4f37160d67f5db61addbe56573c5ac670557a9
Thiago <thiagobazinga@gmail.com>
2020-06-03 14:38:34 -0300

    Desafio Módulo 2 - exercicio 2

--- 2_manipulandoDom/exercicios/scripts.js
+++ 2_manipulandoDom/exercicios/scripts.js
@@ -3,14 +3,35 @@ let divApp = document.querySelector('#app')
 
 let textoBotao = document.createTextNode('Clique para adicionar')
 
-// let addQuadrado = 
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 
 botaoAdicionar.appendChild(textoBotao);
 botaoAdicionar.onclick = function () {
     let quadrado = document.createElement('div');
+
     quadrado.style.width = '100px';
     quadrado.style.height = '100px';
     quadrado.style.backgroundColor = '#F00';
+
+    quadrado.addEventListener("mouseover",() => {
+        let newColor = getRandomColor();
+
+        quadrado.style.background = newColor;
+
+    });
+
+    quadrado.classList.add('square')
+    
     divApp.appendChild(quadrado)
 };
+
+
 
