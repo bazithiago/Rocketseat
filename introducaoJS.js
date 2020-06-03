f75963186f36d1a75f072e8c02e6eae53c66c287
Thiago <thiagobazinga@gmail.com>
2020-06-02 23:48:42 -0300

    Módulo 1 - exercicio 5. for of

--- introducaoJS.js
+++ introducaoJS.js
@@ -1,21 +1,15 @@
-function experiencia(anos) {
-
-    if (anos < 1) {
-        return "Iniciante"
-    } else if (anos >= 1 && anos <=3 ) {
-        return "Intermediário"
-    } else if (anos >= 4 && anos <= 6) {
-        return "Avançado"
-    } else {
-        return "Jedi Master"
+var usuarios = [
+    {
+        nome: "Diego",
+        habilidades: ["Javascript", "ReactJS", "Redux"]
+    },
+    {
+        nome: "Gabriel",
+        habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
     }
-
-   }
-   var anosEstudo = 4;
-   console.log(experiencia(anosEstudo));
+];
 
 
-   // De 0-11 meses: Iniciante
-// De 1-3 anos: Intermediário
-// De 3-6 anos: Avançado
-// De 7 acima: Jedi Master
+for (let usuario of usuarios){
+    console.log('O ' + usuario.nome + ' possui as habilidades: ' + usuario.habilidades.join(", "));
+}
