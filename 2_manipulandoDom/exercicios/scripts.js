6dfab9375a32fb62e80d47ff0522fb77a33f9c5c
Thiago <thiagobazinga@gmail.com>
2020-06-03 11:56:09 -0300

    Alterando estilos com JS

--- 2_manipulandoDom/exercicios/scripts.js
+++ 2_manipulandoDom/exercicios/scripts.js
@@ -1,8 +1,14 @@
 let inputElement = document.querySelector('input[id=nome]')
 let btnElement = document.querySelector('button.botao')
+let boxElement = document.querySelector('#app')
 
 btnElement.onclick = function() {
     let text = inputElement.value;
 
     alert(text)
 }
+
+boxElement.style.width = 100;
+boxElement.style.height = '100px';
+boxElement.style.backgroundColor = '#f00';
+
